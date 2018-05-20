d71bec8cdfc9e8d3f53852b537f2c8e3f9dd4d45
Alireza Teimoori <alireza@ip-172-31-8-86.ca-central-1.compute.internal>
2018-05-20 13:54:51 +0000

    first commit

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+// Created by: Alireza Teimoori
+// Created on: 15 May 2018
+
+import java.util.Scanner;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
